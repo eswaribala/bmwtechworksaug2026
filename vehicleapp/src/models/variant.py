948b9b183bc9d550d4fffcf0d85de0b6a0b1bd94
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-21 15:03:42 +0530

    updated

---
+++ vehicleapp/src/models/variant.py
@@ -0,0 +1,12 @@
+from enum import Enum
+class Variant(Enum):
+    """
+    Enum class representing different variants of a product.
+    Each variant has a unique identifier and a descriptive name.
+    """
+
+    VARIANT_A = "High End"
+    VARIANT_B = "Mid Range"
+    VARIANT_C = "Entry Level"
+
+    
