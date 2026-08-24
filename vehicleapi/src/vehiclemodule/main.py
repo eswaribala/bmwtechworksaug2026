6a1095e13df1e10897719d63b1f21655680f5183
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-24 15:41:20 +0530

    updated

--- vehicleapi/src/vehiclemodule/main.py
+++ vehicleapi/src/vehiclemodule/main.py
@@ -4,7 +4,7 @@ from vehiclemodule.configurations.postgres_conn import base, engine
 from fastapi import FastAPI
 
 
-api=FastAPI(title="Vehicle Management API", 
+app=FastAPI(title="Vehicle Management API", 
             description="API for managing vehicles", 
             version="1.0")
 
@@ -15,4 +15,4 @@ base.metadata.create_all(bind=engine)
 
 
 from vehiclemodule.controllers.vehicle_controller import router
-api.include_router(router)
+app.include_router(router)
