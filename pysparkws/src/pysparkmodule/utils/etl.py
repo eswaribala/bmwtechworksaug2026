bfc27e1c68a4df6493a6db95458a69cf9a96e95c
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-25 14:54:09 +0530

    updated

--- pysparkws/src/pysparkmodule/utils/etl.py
+++ pysparkws/src/pysparkmodule/utils/etl.py
@@ -3,16 +3,18 @@
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, count, when
-
-spark = SparkSession.builder \
-    .appName("ETL Application") \
-    .config("spark.executor.memory", "2g") \
-    .config("spark.driver.memory", "2g") \
-    .config("spark.sql.shuffle.partitions", "4") \
-    .master("local[*]") \
-    .getOrCreate()
+#driver memory and executor memory can be set in the spark session builder
+spark = (SparkSession.builder 
+    .appName("ETL Application") 
+    .config("spark.executor.memory", "2g") 
+    .config("spark.driver.memory", "2g") 
+    .config("spark.sql.shuffle.partitions", "4") 
+    #cluster mode can be set to local[*] to use all the available cores in the local machine
+    .master("local[*]") 
+    .getOrCreate())
 
 #read customer data from csv file
+#executors
 customer_df = spark.read.csv("src/pysparkmodule/data/bmw_customers_1000.csv", header=True, inferSchema=True)
 
 #data cleaning
@@ -39,6 +41,12 @@ customer_df = customer_df.withColumn("purchase_category",
 #filter the customer data based purchase_date between 2023 to 2025
 customer_df = customer_df.filter((col("purchase_date") >= "2023-01-01") 
                                  & (col("purchase_date") <= "2025-12-31"))
-
+#action
 #save as csv file using pandas to avoid the error "Py4JJavaError: An error occurred while calling o42.csv. : org.apache.spark.SparkException: Task not serializable"
-customer_df.toPandas().to_csv("src/pysparkmodule/data/bmw_customers_cleaned.csv", index=False)
+#customer_df.toPandas().to_csv("src/pysparkmodule/data/bmw_customers_cleaned.csv", index=False)
+#save as parquet file
+customer_df.toPandas().to_parquet("src/pysparkmodule/data/bmw_customers_cleaned.parquet", engine="pyarrow", index=False)
+#show pyspark ui
+print("Pyspark UI is available at: http://localhost:4040")
+input("Press Enter to exit...")
+spark.stop()
