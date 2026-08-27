6e3111f6242138caa5b89cf0714f61ebd2ffc989
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-27 10:28:57 +0530

    updated

--- pysparkws/src/pysparkmodule/utils/rdd_transformations.py
+++ pysparkws/src/pysparkmodule/utils/rdd_transformations.py
@@ -1,45 +1,60 @@
-
-from pyspark import TaskContext
 from pyspark.sql import SparkSession
 
-#create the driver
-spark = (SparkSession.builder
-         .appName("MyApp")
-         .config("spark.sql.shuffle.partitions", "4")
-         .config("spark.executor.memory", "2g")
-         #cluster mode
-         .master("local[4,4]")
-         .getOrCreate()
-        )
+spark = (
+    SparkSession.builder
+    .appName("BMW Sensor RDD")
+    .config("spark.executor.memory", "2g")
+    .master("local[*]")
+    .getOrCreate()
+)
 
-#create the RDD
 context = spark.sparkContext
 
-#read the text file
-#data  partitions
-rdd = context.textFile("src/pysparkmodule/data/bmw_sensor_raw_data.txt", 
-                       minPartitions=4)
-#remove the header from the RDD
+# Read BMW sensor raw data
+rdd = context.textFile(
+    "src/pysparkmodule/data/bmw_sensor_raw_data.txt",
+    minPartitions=4
+)
+
+# Remove header
 header = rdd.first()
 
-split_rdd = rdd.filter(lambda x: x != header).map(lambda x: x.split("#"))
+split_rdd = (
+    rdd
+    .filter(lambda x: x != header)
+    .map(lambda x: x.split("#"))
+)
 
-#count the number of records in the RDD
-count = split_rdd.count()
-print(f"Number of records in the RDD: {count}")
+print(f"Number of records in the RDD: {split_rdd.count()}")
+print(f"Number of partitions in the RDD: {split_rdd.getNumPartitions()}")
 
-#count the number of partitions in the RDD
-num_partitions = split_rdd.getNumPartitions()
+# ---------------------------------------------------
+# vehicle_id = column 0
+# engine_rpm = column 3
+# ---------------------------------------------------
 
-print(f"Number of partitions in the RDD: {num_partitions}")
+rpm_rdd = split_rdd.map(
+    lambda x: (x[0], int(x[3]))
+)
 
-#group the cars by vehicle_id column 0
-split_rdd_with_key = split_rdd.map(lambda x: (x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8],x[9],x[10],x[11],x[12],x[13],x[14],x[15],x[16],x[17],x[18],x[19],x[20],x[21],x[22]))     
+print("Sample key-value pairs:")
+for row in rpm_rdd.take(5):
+    print(row)
 
-grouped_rdd = split_rdd_with_key.groupByKey()
+# Group RPM values by vehicle
+grouped_rdd = rpm_rdd.groupByKey()
 
-#find average engine rpm
-average_rpm_by_car = grouped_rdd.mapValues(lambda records: sum(int(record[1]) for record in records) / len(records))
+# Calculate average RPM
+average_rpm_by_car = grouped_rdd.mapValues(
+    lambda rpms: sum(rpms) / len(rpms)
+)
 
-for car_id, avg_rpm in average_rpm_by_car.collect():
-    print(f"Car ID: {car_id}, Average Engine RPM: {avg_rpm}")
+print("\nAverage Engine RPM by BMW:")
+
+for adas, avg_rpm in average_rpm_by_car.collect():
+    print(
+        f"ADAS: {adas}, "
+        f"Average Engine RPM: {avg_rpm:.2f}"
+    )
+
+spark.stop()
