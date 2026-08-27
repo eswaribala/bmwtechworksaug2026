0b03a8052756da7690c841adc1d6a6c18d59d945
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-27 12:26:27 +0530

    updated

---
+++ pysparkws/src/pysparkmodule/utils/rdd_join.py
@@ -0,0 +1,44 @@
+from pyspark.sql import SparkSession
+
+
+spark = (SparkSession.builder
+         .appName("BMW Sensor Telemetry")
+         #define cluster configuration
+         .master("local[*]")
+         .config("spark.executor.memory", "2g")
+         .getOrCreate())
+
+#rdd
+sc=spark.sparkContext
+
+#read sales data from a CSV file
+
+sales_data = sc.textFile("src/pysparkmodule/data/bmw_sales_raw.csv",
+                        minPartitions=4)
+
+header=sales_data.first()
+
+filtered_sales_data = sales_data.filter(lambda line: line != header)
+
+#key value pair for sales data
+
+sales_kv = (filtered_sales_data.map(lambda line: line.split(",")) 
+           .map(lambda fields: (fields[2], (fields[1],fields[4]))))  # Assuming the first column is the key
+
+master_data = sc.textFile("src/pysparkmodule/data/master_data.csv",
+                        minPartitions=4)
+
+header_master=master_data.first()
+
+filtered_master_data = master_data.filter(lambda line: line != header_master)
+
+master_kv = (filtered_master_data.map(lambda line: line.split(","))
+           .map(lambda fields: (fields[0], (fields[1],fields[2]))))  # Assuming the first column is the key
+
+#join the sales data with master data using the key
+
+joined_data = sales_kv.join(master_kv)
+
+#print("Joined Data:")
+for line in joined_data.collect():
+    print(line)
