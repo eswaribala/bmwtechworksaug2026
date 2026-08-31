fa0f9a40bb0f1839d7bc3a619a274fea1d34aa64
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-31 17:46:57 +0530

    updated

---
+++ awsws/src/awsmodule/utils/pysparkpg.py
@@ -0,0 +1,21 @@
+#step1 
+from pyspark.sql import SparkSession
+from awsmodule.configurations.config import Config
+#create driver
+spark = (
+        SparkSession.builder
+        .appName("Spark PostgreSQL Example")
+        .config("spark.jars.packages", "org.postgresql:postgresql:42.2.18")  # Path to the PostgreSQL JDBC driver
+        #cluster mode
+        .master("local[*]")  # Use local mode for testing; adjust as needed for your cluster
+        .getOrCreate()
+      )
+#create jdbc url
+jdbc_url = Config().get_jdbc_connection_string()
+
+result=spark.read.jdbc(url=jdbc_url, 
+                table="users", 
+                properties=Config().get_db_connection_params())
+
+print("Data read from PostgreSQL:")
+result.show()
