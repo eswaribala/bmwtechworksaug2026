7138e44760ee300b9738813a4fc6b353165939d7
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-09-04 12:59:05 +0530

    updated

--- genaiws/src/promptmodule/utils/zeroshotllm.py
+++ genaiws/src/promptmodule/utils/zeroshotllm.py
@@ -6,11 +6,11 @@ env_path=os.path.join(os.path.dirname(__file__),'..', '.env')
 load_dotenv(env_path)
 
 #setup OpenAI client
-client = OpenAI(api_key=os.getenv("openai_ai_key"))
+#client = OpenAI(api_key=os.getenv("openai_ai_key"))
 
 #use gemini llm
-#gemini_api_key=os.getenv("gemini_api_key")
-#client = genai.Client(api_key=gemini_api_key)
+gemini_api_key=os.getenv("gemini_api_key")
+client = genai.Client(api_key="AIzaSyD-GlttBbz-Y9GzqCcgk5FnOm_ACqm9qoQ")
 
 zero_shot_prompt="""
 Tata Nexon EV
@@ -19,12 +19,17 @@ Battery Level= 5%
 Temperature= 88°C
 return only the classification
 """
+'''
 result=client.responses.create(
     model="gpt-4.1-mini",
     input=zero_shot_prompt
 )
-
-print(result.output_text)
+'''
+response = client.models.generate_content(
+    model="gemini-2.5-flash-lite",
+    contents=zero_shot_prompt
+)
+print(response.text)
 
 
 
