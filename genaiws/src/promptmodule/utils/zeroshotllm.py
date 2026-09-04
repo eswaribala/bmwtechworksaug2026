ecd6d84b8e72a399e8ba0779acd9ba8b6c351611
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-09-04 12:40:33 +0530

    updated

--- genaiws/src/promptmodule/utils/zeroshotllm.py
+++ genaiws/src/promptmodule/utils/zeroshotllm.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+from google import genai
 from openai import OpenAI   
 env_path=os.path.join(os.path.dirname(__file__),'..', '.env')
 load_dotenv(env_path)
@@ -7,7 +8,9 @@ load_dotenv(env_path)
 #setup OpenAI client
 client = OpenAI(api_key=os.getenv("openai_ai_key"))
 
-#test the client
+#use gemini llm
+gemini_api_key=os.getenv("gemini_api_key")
+client = genai.Client(api_key=gemini_api_key)
 
 
 
