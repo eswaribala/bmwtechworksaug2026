3f0765fd0ad5c470cae3c1ac306c253bcf06ec4a
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-09-04 13:01:16 +0530

    updated

--- genaiws/src/promptmodule/utils/zeroshotllm.py
+++ genaiws/src/promptmodule/utils/zeroshotllm.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 from google import genai
 from openai import OpenAI   
-env_path=os.path.join(os.path.dirname(__file__),'..', '.env')
+env_path=os.path.join(os.path.dirname(__file__),'..','..','..', '.env')
 load_dotenv(env_path)
 
 #setup OpenAI client
@@ -10,7 +10,8 @@ load_dotenv(env_path)
 
 #use gemini llm
 gemini_api_key=os.getenv("gemini_api_key")
-client = genai.Client(api_key="AIzaSyD-GlttBbz-Y9GzqCcgk5FnOm_ACqm9qoQ")
+print(gemini_api_key)
+client = genai.Client(api_key=gemini_api_key)
 
 zero_shot_prompt="""
 Tata Nexon EV
