c94cefb59480afff75bc6715f92d4da62632e8ef
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-09-15 17:44:45 +0530

    updated

--- kafkaws/kafkaproduceragent/src/kafkaapp/pyspark_kafka.py
+++ kafkaws/kafkaproduceragent/src/kafkaapp/pyspark_kafka.py
@@ -10,3 +10,23 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 KRAFT_BOOTSTRAP_SERVERS = os.getenv("KRAFT_BOOTSTRAP_SERVERS")
 KAFKA_AVRO_TOPIC = os.getenv("KAFKA_AVRO_TOPIC")
+
+#define spark session
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DateType
+
+spark = (SparkSession.builder 
+    .appName("KafkaSparkApp") 
+    .master("local[*]") 
+    .config("spark.sql.shuffle.partitions", "4")
+    .getOrCreate())
+
+spark.sparkContext.setLogLevel("INFO")
+
+customer_data_struct= StructType([
+    StructField("customer_id", IntegerType(), True),
+    StructField("customer_name", StringType(), True),
+    StructField("address", StringType(), True),
+    StructField("email", StringType(), True),
+    StructField("dob", DateType(), True)
+])
